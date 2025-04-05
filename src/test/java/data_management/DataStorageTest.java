11faec1cb3eda0ff095d524a50eae28b2a721fb1
irina_kalmykova <irina.kalmykova@student.uhasselt.be>
2025-04-05 22:31:48 +0200

    First assignment done

--- src/test/java/data_management/DataStorageTest.java
+++ src/test/java/data_management/DataStorageTest.java
@@ -1,20 +1,32 @@
 package data_management;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Test;
+//import org.mockito.Mock;
 
 import com.data_management.DataStorage;
 import com.data_management.PatientRecord;
+//import com.data_management.DataReader;
 
 import java.util.List;
 
 class DataStorageTest {
 
+    // ik added mock data reader to satisfy TODO below
+//    @Mock
+//    private DataReader reader;  // Mock reader instead of using a real one
+
     @Test
     void testAddAndGetRecords() {
         // TODO Perhaps you can implement a mock data reader to mock the test data?
+        // ik - added mock data deader
+//        reader = mock(DataReader.class); //check later if needed at all
         // DataReader reader
-        DataStorage storage = new DataStorage(reader);
+        //DataStorage storage = new DataStorage(reader);
+        DataStorage storage = new DataStorage();
+
         storage.addPatientData(1, 100.0, "WhiteBloodCells", 1714376789050L);
         storage.addPatientData(1, 200.0, "WhiteBloodCells", 1714376789051L);
 
